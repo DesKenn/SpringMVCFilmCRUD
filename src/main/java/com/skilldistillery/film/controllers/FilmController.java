aad142cb2445ce55041abe9ebc34e9454788c3c0
DesKenn <destineek423@gmail.com>
2024-08-09 16:52:29 -0600

    configure Spring

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,12 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class FilmController {
+	@RequestMapping (path= {"index.do", "/"})
+	public String index() {
+		return "WEB-INF/index.jsp";
+	}
 
 }
