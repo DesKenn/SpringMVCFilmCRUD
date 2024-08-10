9195c65f2020cfab6e6064a7d2dbd6bfc06e4953
DesKenn <destineek423@gmail.com>
2024-08-10 15:17:48 -0600

    added some methods

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -2,18 +2,89 @@ package com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.dao.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
 
 	@Autowired
-	private FilmDAO filmDao;
+	private FilmDAO filmDAO;
 
-	@RequestMapping (path= {"index.do", "/"})
+	@RequestMapping("index.do")
 	public String index() {
 		return "WEB-INF/index.jsp";
 	}
+	@RequestMapping(path = "index.do", params = "id", method = RequestMethod.GET)
+	public ModelAndView findFilmById(String id) {
+		ModelAndView mv = new ModelAndView();
+		Film film = null;
+		try {
+			int Id = Integer.parseInt(id);
+			film = filmDAO.findFilmById(Id);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		 if (film == null) {
+			mv.setViewName("WEB-INF/index.jsp");
+		} else {
+			mv.addObject("info", film);
+			mv.setViewName("/WEB-INF/index.jsp");
+		}
+
+		return mv;
+	}
+	@RequestMapping(path = "index.do", method = RequestMethod.GET)
+	private ModelAndView addFilm( Film film) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/WEB-INF/index.jsp");
+		
+		return mv;
+		
+	}
+	@RequestMapping(path = "index.do", method = RequestMethod.POST)
+	public ModelAndView createFilm(Film film, Errors error) {
+		ModelAndView mv = new ModelAndView();
+		
+		if(error.getErrorCount()>0) {
+			mv.setViewName("/WEB-INF/index.jsp");
+		}
+		
+		else {
+			Film newFilm = filmDAO.addFilm(film);
+			mv.addObject("newfilm", newFilm);
+			mv.setViewName("WEB-INF/index.jsp");
+			
+		}
+		return mv;
+
+	}
+	
+
+//	@RequestMapping(path = { "index.do"}, method = RequestMethod.GET )
+//	public ModelAndView findById(@RequestParam("id") int id) {
+//		ModelAndView mv = new ModelAndView();
+//		Film f = filmDAO.findFilmById(1);
+//		mv.addObject("film", f);
+//		mv.setViewName("WEB-INF/index.jsp");
+//		return mv;
+//
+//	}
+////	@RequestMapping (path= "index.do", params ="id", method =RequestMethod.GET)
+////	public ModelAndView findById(@RequestParam("id")int id ) {
+////		 ModelAndView mv = new ModelAndView();
+////		  Film f = filmDAO.findFilmById(1);
+////		  mv.addObject("film", f);
+////		  mv.setViewName("WEB-INF/index.jsp");
+////				return mv;
+////		
+////	}
+
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib uri="jakarta.tags.core" prefix="c" %>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="jakarta.tags.core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +8,56 @@
 <title>Welcome to the MVC Film Site</title>
 </head>
 <body>
-<h1>Welcome to the Film Site</h1>
+	<h1>Welcome to the Film Site</h1>
 
+	<form action="index.do" method="GET">
+		Enter a film ID: <input type="text" name="film id" /> <input
+			type="submit" value="Submit" />
+	</form>
+
+	<h1>Add A New Film</h1>
+	<form action="index.do" method="POST" modelAttribute="film">
+
+		<form:label path="title">Title: </form:label>
+		<br>
+		<form:input class="input" path="title" placeholder="Enter Title" />
+		<form:errors path="title" />
+
+		<br>
+		<form:label path="description">Description:</form:label>
+		<br>
+		<form:input class="input" path="description"
+			placeholder="Enter Description" />
+		<br>
+		<form:label path="releaseYear">Release Year:</form:label>
+		<br>
+		<form:input class="input" path="releaseYear" min="0" max="2040"
+			type="number" placeholder="Year" />
+		<form:errors path="releaseYear" />
+		<br>
+		<form:label path="rentalDuration">Rental Duration:</form:label>
+		<br>
+		<form:input class="input" path="rentalDuration" min="0" type="number"
+			placeholder="Enter Days" />
+		<form:errors path="rentalDuration" />
+		<br>
+		<form:label path="length">Length:</form:label>
+		<br>
+		<form:input class="input" path="length" min="0" type="number"
+			placeholder="Enter Length In Minutes" />
+		<form:errors path="length" />
+		<br>
+		<form:label path="replacementCost">Replacement Cost:</form:label>
+		<br>
+		<form:input class="input" path="replacementCost" type="number" min="0"
+			placeholder="Enter Cost" />
+		<br>
+		<form:label path="rating">Rating:</form:label>
+		<br>
+		<form:input class="input" path="rating" required="required"
+			placeholder="Enter Rating" />
+		<form:errors path="rating" />
+		<br>
+	</form>
 </body>
 </html>
